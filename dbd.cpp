d12215cb7b92824514bc307717d327d3826565ac
VON0222 <88916058+VON0222@users.noreply.github.com>
2022-06-01 15:27:27 -0500

    Add files via upload

--- dbd.cpp
+++ dbd.cpp
@@ -1,4 +1,7 @@
-#include "dbd.h"
+#include "Asesino.h"
+#include "Superviviente.h"
+#include "MapaExt.h"
+#include "MapaInt.h"
 #include <iostream>
 
 using namespace std;
@@ -8,9 +11,26 @@ int main(){
     Asesino Trapper("Trapper", 1.80, 100, 32, "Trampas");
     cout<<default1.getVolumen()<<endl;
     Trapper.saltarVentana();
+    Trapper.AtaqueS();
 
     Superviviente default2;
     Superviviente Jake("Jake", 1.60, 90, "sano");
+    Superviviente Meg("Meg", 1.55, 105, "sano");
+    Superviviente Dwight("Dwight", 1.60, 95, "sano");
+    Superviviente Claudette("Claudette", 1.55, 105, "sano");
     cout<<default2.getVolumen()<<endl;
     Jake.saltarVentana();
+    Meg.caminar();
+    Dwight.Herir();
+    Claudette.interactuarPale();
+
+    MapaInt RPD(20,3);
+    RPD.GenerarMapa();
+    RPD.ColocarPersonajes();
+    RPD.Collapsar();
+
+    MapaExt GardenOfJoy(24,"Casa Retorcida");
+    GardenOfJoy.GenerarMapa();
+    GardenOfJoy.ColocarPersonajes();
+    GardenOfJoy.Collapsar();
 }
